b3a066a8a2ae468569cc31edca663792e42c9716
Chatchai Saratakij <CSaratakij@gmail.com>
2018-05-15 02:26:23 +0700

    Init Project.

---
+++ Assets/Scripts/InteractableBananaHandler.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using VRStandardAssets.Utils;
+
+public class InteractableBananaHandler : MonoBehaviour
+{
+    [SerializeField]
+    VRInteractiveItem interactiveItem;
+
+    [SerializeField]
+    VRInput vrInput;
+
+
+    public delegate void Func();
+
+    public event Func OnVisible;
+    public event Func OnInvisible;
+
+
+
+    bool isGazeOver;
+
+    AudioSource audioSource;
+    MeshRenderer meshRenderer;
+    MeshCollider meshCollider;
+
+
+    void Awake()
+    {
+        _Initialize();
+    }
+
+    void OnEnable()
+    {
+        _Subscribe_Events();
+    }
+
+    void OnDisable()
+    {
+        _Unsubscribe_Events();
+    }
+
+    void _Initialize()
+    {
+        audioSource = GetComponent<AudioSource>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshCollider = GetComponent<MeshCollider>();
+    }
+
+    void _Subscribe_Events()
+    {
+        vrInput.OnDown += HandleDown;
+        vrInput.OnUp += HandleUp;
+
+        interactiveItem.OnOver += HandleOver;
+        interactiveItem.OnOut += HandleOut;
+    }
+
+    void _Unsubscribe_Events()
+    {
+        vrInput.OnDown -= HandleDown;
+        vrInput.OnUp -= HandleUp;
+
+        interactiveItem.OnOver -= HandleOver;
+        interactiveItem.OnOut -= HandleOut;
+    }
+
+    void _FireEvent_OnVisible()
+    {
+        if (OnVisible != null)
+        {
+            OnVisible();
+        }
+    }
+
+    void _FireEvent_OnInvisible()
+    {
+        if (OnInvisible != null)
+        {
+            OnInvisible();
+        }
+    }
+
+    void HandleDown()
+    {
+        if (isGazeOver && meshRenderer.enabled)
+        {
+            audioSource.Play();
+            Show(false);
+        }
+    }
+
+    void HandleUp()
+    {
+
+    }
+
+    void HandleOver()
+    {
+        isGazeOver = true;
+    }
+
+    void HandleOut()
+    {
+        isGazeOver = false;
+    }
+
+    public void Show(bool value)
+    {
+        meshRenderer.enabled = value;
+        meshCollider.enabled = value;
+
+        if (value)
+        {
+            _FireEvent_OnVisible();
+        }
+        else
+        {
+            _FireEvent_OnInvisible();
+        }
+    }
+}
---
+++ Assets/Scripts/RespawnHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RespawnHandler : MonoBehaviour
+{
+    [SerializeField]
+    float maxRespawnTime;
+
+
+    InteractableBananaHandler bananaHandler;
+
+
+    void Awake()
+    {
+        bananaHandler = GetComponent<InteractableBananaHandler>();
+    }
+
+    void OnEnable()
+    {
+        bananaHandler.OnInvisible += _OnInvisible;
+    }
+
+    void OnDestroy()
+    {
+        bananaHandler.OnInvisible -= _OnInvisible;
+    }
+
+    void _OnInvisible()
+    {
+        StartCoroutine(_Respawn_Callback(maxRespawnTime));
+    }
+
+    IEnumerator _Respawn_Callback(float time)
+    {
+        yield return new WaitForSeconds(time);
+        bananaHandler.Show(true);
+    }
+}
